5f2cf7b3dac8f92d36b4e95898e6ec0e84e73f3d
Surendhar <studyspace.ie@gmail.com>
2023-11-11 14:29:49 +0530

    reinitializing the application

--- controllers/addProduct.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-exports.addProduct = (req,res)=>{
-    res.sendFile(path.join(__dirname,'../','views','addProduct.html'))
-}
--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-exports.getIndex = (req,res)=>{
-    res.status(200);
-    res.sendFile(path.join(__dirname, '../', 'views', 'index.html'))
-
-}
